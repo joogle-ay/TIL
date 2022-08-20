e9e21012cf09384b073fcda5c1a81d6c514490e1
Jooeun-K <gangjoo93@gmail.com>
2022-08-20 23:17:48 +0900

    220820 main 수정

---
+++ docs/.vuepress/components/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="titleContainer">
+      <h1>안 쓰면 까먹는다
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import "https://fonts.googleapis.com/css?family=Gugi";
+.infoContainer {
+  text-align: right;
+}
+h1 {
+  font-family: "Gugi", cursive;
+  font-weight: 500;
+  border-bottom: 17px solid #ffd816;
+  word-break: keep-all;
+  font-size: 5rem;
+}
+</style>
