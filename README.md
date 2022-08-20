8a2113a25ab70e7192c17c3d1e7f31d35e78157d
Jooeun-K <gangjoo93@gmail.com>
2022-08-20 20:21:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TIL only blog by VuePress
+
+VuePress로 만든 TIL 전용 블로그입니다.
+간략하게 오늘 배운 것을 기록합니다.
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: "Today I Learned",
+  description: "배운 것을 간략하게 기록합니다.",
+  base: "/TIL/",
+  dest: "build",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/logo.png",
+      },
+    ],
+  ],
+  themeConfig: {
+    nav: [
+      { text: "Github", link: "https://github.com/Jooeun-K", target: "_blank" },
+      { text: "Blog", link: "https://guuumi.tistory.com/", target: "_blank" },
+    ],
+    sidebar: [
+      {
+        title: "TIL",
+        children: ["/TIL/220820"],
+      },
+    ],
+  },
+};
