23865a19a956d3da4ee1ab9dc986af67efb22862
Jooeun-K <gangjoo93@gmail.com>
2022-08-20 22:20:34 +0900

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Today I Learned",
   description: "배운 것을 간략하게 기록합니다.",
-  base: "/",
+  base: "/TIL/",
   dest: "build",
   head: [
     [
@@ -14,6 +14,7 @@ module.exports = {
   ],
   themeConfig: {
     nav: [
+      { text: "About", link: "/about/about-me" },
       { text: "Github", link: "https://github.com/Jooeun-K", target: "_blank" },
       { text: "Blog", link: "https://guuumi.tistory.com/", target: "_blank" },
     ],
