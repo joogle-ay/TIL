1791f809582b1427807b7d71256d07bfba7da4f0
Jooeun-K <gangjoo93@gmail.com>
2022-10-19 20:46:57 +0900

    221019

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const LIST = require("./list");
+const LIST = require('./list');
 
 {
   /* <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
@@ -8,43 +8,47 @@ const LIST = require("./list");
 module.exports = {
   title: "Jooeun's TIL",
   description:
-    "Slow but Steadily. 오늘 배운 것들을 로그 형식으로 작성합니다. 개발 이야기를 주로 합니다. 주니어 프론트엔드 개발자 강주은 A.K.A 쭈꾸미",
-  base: "/TIL/",
+    'Slow but Steadily. 오늘 배운 것들을 로그 형식으로 작성합니다. 개발 이야기를 주로 합니다. 주니어 프론트엔드 개발자 강주은 A.K.A 쭈꾸미',
+  base: '/TIL/',
   head: [
     [
-      "link",
+      'link',
       {
-        rel: "shortcut icon",
-        href: "/assets/favicon/favicon.ico",
-        type: "image/x-icon",
+        rel: 'shortcut icon',
+        href: '/assets/favicon/favicon.ico',
+        type: 'image/x-icon',
       },
     ],
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        href: "/assets/favicon/favicon.ico",
-        type: "image/x-icon",
+        rel: 'icon',
+        href: '/assets/favicon/favicon.ico',
+        type: 'image/x-icon',
       },
     ],
   ],
   themeConfig: {
     nav: [
       // { text: "About", link: "/about/about-me" },
-      { text: "Github", link: "https://github.com/Jooeun-K", target: "_blank" },
-      { text: "Blog", link: "https://guuumi.tistory.com/", target: "_blank" },
+      { text: 'Github', link: 'https://github.com/Jooeun-K', target: '_blank' },
+      { text: 'Blog', link: 'https://guuumi.tistory.com/', target: '_blank' },
     ],
     sidebar: [
       {
-        title: "2022 TIL",
+        title: '2022 TIL',
         children: LIST.TIL_2022,
       },
       {
-        title: "Bookmark",
+        title: '독서 기록',
+        children: LIST.BOOK_JS,
+      },
+      {
+        title: 'Bookmark',
         children: LIST.BOOKMARK,
       },
     ],
     sidebarDepth: 2,
   },
-  plugins: ["@vuepress/back-to-top"],
+  plugins: ['@vuepress/back-to-top'],
 };
