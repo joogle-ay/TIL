3a0d22f74fa0abe90adee3e703a703f0c17a35da
Jooeun-K <gangjoo93@gmail.com>
2022-10-24 23:56:06 +0900

    home.vue 수정

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
     <div class="titleBox">
-      <h1 class="title"><span>Slow but steadily</span>
-      </h1>
+      <h1 class="title"><span>Slow but steadily</span></h1>
     </div>
     <div class="contentBox">
       <h3>This is Jooeun's TIL.</h3>
-      <p>오늘 배운 것들을 로그 형식으로 작성합니다.<br>
-      개발 이야기를 주로 합니다.</p>
+      <p>
+        시야를 계속해서 넓혀나가고 싶은 주니어 프론트엔드 개발자
+        강주은입니다.✨<br />
+        이 곳에는 오늘 배운 것들을 로그 형식으로 작성합니다.<br />
+        정돈된 글, 회고 등은
+        <a href="https://guuumi.tistory.com/" target="_blank">티스토리 블로그</a
+        >에 작성하는 편입니다.
+      </p>
+      <a></a>
     </div>
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
 @font-face {
-    font-family: 'GongGothicMedium';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10@1.0/GongGothicMedium.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
+  font-family: "GongGothicMedium";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10@1.0/GongGothicMedium.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
 }
 .titleBox {
   text-align: left;
-
 }
 h1.title {
   font-family: "GongGothicMedium", "돋움", sans-serif;
@@ -32,11 +37,11 @@ h1.title {
   word-break: keep-all;
   font-size: 3rem;
   padding-bottom: 10px;
-  position:relative;
+  position: relative;
   display: inline-block;
 }
 h1.title span {
-  position:relative;
+  position: relative;
   z-index: 1;
 }
 h1.title:before {
