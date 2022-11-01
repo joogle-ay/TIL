76d1e597ae43766ab90e71f9b017191499a602de
Jooeun-K <gangjoo93@gmail.com>
2022-11-01 23:38:07 +0900

    221101

--- docs/.vuepress/list.js
+++ docs/.vuepress/list.js
@@ -1,8 +1,9 @@
 exports.TIL_2022 = [
-  '/2022/2022-10',
-  '/2022/2022-09',
-  '/2022/2022-08',
-  '/2022/2022-07',
+  "/2022/2022-11",
+  "/2022/2022-10",
+  "/2022/2022-09",
+  "/2022/2022-08",
+  "/2022/2022-07",
 ];
-exports.BOOKMARK = ['/bookmark/2022'];
-exports.BOOK_JS = ['/book-js/js-deepdive'];
+exports.BOOKMARK = ["/bookmark/2022"];
+exports.BOOK_JS = ["/book-js/js-deepdive"];
