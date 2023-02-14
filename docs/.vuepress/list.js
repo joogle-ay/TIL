6a80c1fb66d433bdf1f7b6947d2a7269d5f4eb51
Danny <gangjoo93@gmail.com>
2023-02-14 23:45:52 +0900

    232214

--- docs/.vuepress/list.js
+++ docs/.vuepress/list.js
@@ -6,6 +6,6 @@ exports.TIL_2022 = [
   "/2022/2022-08",
   "/2022/2022-07",
 ];
-exports.TIL_2023 = ["/2023/2023-01"];
+exports.TIL_2023 = ["/2023/2023-02","/2023/2023-01"];
 exports.BOOKMARK = ["/bookmark/2022", "/bookmark/blogs"];
 exports.BOOK_JS = ["/book-js/js-deepdive", "/book-js/network-for-everyone"];
