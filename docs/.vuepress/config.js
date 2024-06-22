e13caae83f8006e509ef2b72a26177e1040b6432
Jooeun-K <gangjoo93@gmail.com>
2024-06-23 03:36:47 +0900

    240623

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,8 +7,7 @@ const LIST = require("./list");
 
 module.exports = {
   title: "Jooeun's TIL",
-  description:
-    "Slow but Steadily. 오늘 배운 것들을 로그 형식으로 작성합니다. 개발 이야기를 주로 합니다. 주니어 프론트엔드 개발자 강주은 A.K.A 쭈꾸미",
+  description: "Slow but Steadily. 오늘 배운 것들을 로그 형식으로 작성합니다. 개발 이야기를 주로 합니다.",
   base: "/TIL/",
   head: [
     [
